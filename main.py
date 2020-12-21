f15271da1259b4a57f2c0edbeff9be156034ccd2
SamroodAli <samroodali@live.com>
2020-12-21 22:41:53 +0530

    Add: turtle instances to seperate file

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
 import turtle
 import random
+from turtles import turtles
 
 scr = turtle.Screen()
-
-red_turtle = turtle.Turtle()
-blue_turtle = turtle.Turtle()
-purple_turtle = turtle.Turtle()
-orange_turtle = turtle.Turtle()
-green_turtle = turtle.Turtle()
-
-red_turtle.name = "Red"
-blue_turtle.name="Blue"
-purple_turtle.name = "Purple"
-orange_turtle.name = "Orange"
-green_turtle.name = "Green"
-
-green_turtle.color("green")
-red_turtle.color("red")
-blue_turtle.color("blue")
-purple_turtle.color("purple")
-orange_turtle.color("orange")
-
-turtles = [red_turtle, blue_turtle, purple_turtle, orange_turtle, green_turtle]
 y = 100
 for turtle in turtles:
     turtle.penup()
@@ -38,6 +19,5 @@ while not won:
         won = True
         winner = racer
 print(winner.name)
-
 
 scr.exitonclick()
---
+++ turtles.py
@@ -0,0 +1,21 @@
+import turtle
+
+red_turtle = turtle.Turtle()
+blue_turtle = turtle.Turtle()
+purple_turtle = turtle.Turtle()
+orange_turtle = turtle.Turtle()
+green_turtle = turtle.Turtle()
+
+red_turtle.name = "Red"
+blue_turtle.name="Blue"
+purple_turtle.name = "Purple"
+orange_turtle.name = "Orange"
+green_turtle.name = "Green"
+
+red_turtle.color("red")
+blue_turtle.color("blue")
+purple_turtle.color("purple")
+orange_turtle.color("orange")
+green_turtle.color("green")
+
+turtles = [red_turtle, blue_turtle, purple_turtle, orange_turtle, green_turtle]
