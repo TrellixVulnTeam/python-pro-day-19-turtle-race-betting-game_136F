4965f79bde524161f361847e67d8ff1f49baf18e
SamroodAli <samroodali@live.com>
2020-12-21 22:14:05 +0530

    Add: four turtle customization and positioning

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import turtle
 import random
 
+scr = turtle.Screen()
+
 Raphael = turtle.Turtle()
 Leonardo = turtle.Turtle()
 Donatello = turtle.Turtle()
 Michelangelo = turtle.Turtle()
+Roshi = turtle.Turtle()
 
+Roshi.color("green")
+Raphael.color("red")
+Leonardo.color("blue")
+Donatello.color("purple")
+Michelangelo.color("orange")
+
+turtles = [Raphael, Leonardo, Donatello, Michelangelo, Roshi]
+y = 100
+for turtle in turtles:
+    turtle.penup()
+    turtle.shape("turtle")
+    turtle.setposition(-400, y)
+    y -= 50
+
+scr.exitonclick()
