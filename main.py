08f1e18999d474c269a2ee1132e27f4c977c871f
SamroodAli <samroodali@live.com>
2020-12-21 22:52:56 +0530

    Add: user input and checking if user won or not

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 from turtles import turtles
 
 scr = turtle.Screen()
+scr.title("Turtle Race Betting Game")
 y = 100
 for turtle in turtles:
     turtle.penup()
@@ -10,14 +11,20 @@ for turtle in turtles:
     turtle.setposition(-400, y)
     y -= 50
 
+user_choice_turtle = scr.textinput("Choose your turtle", "Which turtle are you betting on ?\n [Red , Blue , Purple , "
+                                                         "Orange , Green").lower()
+
 won = False
 while not won:
     racer = random.choice(turtles)
     racer.forward(10)
-    print(racer.position())
     if racer.xcor() >= 400:
         won = True
         winner = racer
-print(winner.name)
+        if winner.name.lower() == user_choice_turtle:
+            print("Your turtle won, congrats")
+        else:
+            print("Sorry, your turtle lost")
+        print(f"The winner was {winner.name}")
 
 scr.exitonclick()
